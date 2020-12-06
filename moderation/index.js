a1f91c4d5ceeea31f4755e516bcf2016c9f9a91f
Yassine <yassin23raki@gmail.com>
2020-12-07 00:30:56 +0100

    Moderation service Done.

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,23 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {});
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        ...data,
+        status,
+      },
+    });
+  }
+
+  res.send({});
+});
 
 app.listen(4003, () => {
   console.log('Listening on port 4003');
