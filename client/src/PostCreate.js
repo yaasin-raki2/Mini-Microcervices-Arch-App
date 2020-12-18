76e82339269d8293864ed45429ac527a1b488d32
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-18 21:41:26 +0100

    Setting up Skaffold to automate the shity repetetive stuff I've been doing the whole time -_-

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async event => {
     event.preventDefault();
 
-    await axios.post('http://posts.com/posts', { title });
+    await axios.post('http://posts.com/posts/create', { title });
 
     setTitle('');
   };
